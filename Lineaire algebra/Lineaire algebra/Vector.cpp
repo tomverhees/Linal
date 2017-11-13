bcf07f4c6015e25df0806517956cd71cf6ee8051
Tom Verhees <tom_verhees1@hotmail.com>
2017-11-13 12:11:11 +0100

    Vector optimized

--- Lineaire algebra/Lineaire algebra/Vector.cpp
+++ Lineaire algebra/Lineaire algebra/Vector.cpp
@@ -1,5 +1,16 @@
 #include "Vector.h"
 
+Vector::Vector(int length, int direction)
+{
+	this->length = length; 
+	this->direction = direction; 
+}
+
+Vector::~Vector()
+{
+	delete v; 
+}
+
 void Vector::setdirection(int value)
 {
 	direction = value; 
@@ -22,32 +33,25 @@ int Vector::getdirection()
 
 void Vector::addVectors(int richting, int lengte)
 {
-	Vector v; 
-	v.setLength(lengte);
-	v.setdirection(richting);
+	v = new Vector(lengte, richting);
 }
 
-Vector Vector::scaleVector(int value)
+Vector* Vector::scaleVector(int value)
 {
-	Vector v; 
-	v.setdirection(direction * value); 
-	v.setLength(length * value);
+	v->setdirection(direction * value); 
+	v->setLength(length * value);
 	return v;
 }
 
-Vector Vector::combineVectors(Vector v)
+Vector* Vector::combineVectors(Vector &v)
 {
-	Vector m; 
-	m.setLength(length + v.getlength()); 
-	m.setdirection(direction + v.getdirection()); 
+	Vector* m = new Vector(this->length + v.getlength(), this->direction + v.getdirection()); 
 	return m; 
 }
 
-Vector Vector::subtractVectors(Vector v)
+Vector* Vector::subtractVectors(Vector &v)
 {
-	Vector m;
-	m.setLength(length - v.getlength());
-	m.setdirection(direction - v.getdirection());
+	Vector* m = new Vector(this->length - v.getlength(), this->direction - v.getdirection());
 	return m;
 }
 
