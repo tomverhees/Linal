fc4d0b3d4a6fb62d9c0231fe434cfe2724a73653
Bart Dierx <bartdierx@gmail.com>
2018-01-11 19:27:03 +0100

    Added SDL in project files

--- Lineaire algebra/Lineaire algebra/Lineaire algebra.cpp
+++ Lineaire algebra/Lineaire algebra/Lineaire algebra.cpp
@@ -8,9 +8,9 @@
 
 int main()
 {
-	std::vector<float> array; 
-	 //
-	for(int i = 0; i< 9; i++)
+	std::vector<float> array;
+	//
+	for (int i = 0; i < 9; i++)
 	{
 		array.push_back(3);
 	}
@@ -19,8 +19,8 @@ int main()
 		std::cout << array[j] << " ";
 	}
 	std::cout << std::endl;
-	
-	Matrix<float> m = Matrix<float>(3,2); 
+
+	Matrix<float> m = Matrix<float>(3, 2);
 	/*for (int i = 0; i < m.xlength(); i++) {
 		m(i, 0) = 5;
 	}
@@ -28,15 +28,15 @@ int main()
 		m(i, 1) = 3;
 	}*/
 
-	m(0, 0) = 2; 
+	m(0, 0) = 2;
 	m(0, 1) = 2;
 	m(1, 0) = 2;
-	m(1, 1) = 2; 
+	m(1, 1) = 2;
 	m(2, 0) = 2;
 	m(2, 1) = 2;
 	Matrix<float> n = Matrix<float>(3, 3);
-	auto k = n.translate2d(0.1, 0.1); 
+	auto k = n.translate2d(0.1, 0.1);
 	auto l = m.rotate3dall(10, 5, 2, 3);
 	auto f = m * k;
-    return 0;
+	return 0;
 }
